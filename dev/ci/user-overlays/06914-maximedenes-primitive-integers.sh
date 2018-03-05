10b1e383edef9b21c91d3d454e828123b0548298
Maxime Dénès <mail@maximedenes.fr>
2018-03-05 11:09:51 +0100

    Overlays.

---
+++ dev/ci/user-overlays/06914-maximedenes-primitive-integers.sh
@@ -0,0 +1,4 @@
+if [ "$CI_PULL_REQUEST" = "6914" ] || [ "$CI_BRANCH" = "primitive-integers" ]; then
+    bignums_CI_BRANCH=primitive-integers
+    bignums_CI_GITURL=https://github.com/maximedenes/bignums.git
+fi
