f7a03f1c781fb1384729999606d6a12b4bc2d574
Maxime Dénès <mail@maximedenes.fr>
2018-03-05 11:09:51 +0100

    Overlays.

---
+++ dev/ci/user-overlays/06914-maximedenes-primitive-integers.sh
@@ -0,0 +1,12 @@
+if [ "$CI_PULL_REQUEST" = "6914" ] || [ "$CI_BRANCH" = "primitive-integers" ]; then
+
+    bignums_CI_REF=primitive-integers
+    bignums_CI_GITURL=https://github.com/vbgl/bignums
+
+    CompCert_CI_REF=primitive-integers
+    CompCert_CI_GITURL=https://github.com/vbgl/CompCert
+
+    mtac2_CI_REF=primitive-integers
+    mtac2_CI_GITURL=https://github.com/vbgl/Mtac2
+
+fi
