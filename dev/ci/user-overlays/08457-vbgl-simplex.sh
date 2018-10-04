7574659faa44b53338388a3260ccdd665097cc08
Vincent Laporte <Vincent.Laporte@fondation-inria.fr>
2018-10-04 14:47:36 +0000

    Make overlay work on my gitlab.com fork

--- dev/ci/user-overlays/08457-vbgl-simplex.sh
+++ dev/ci/user-overlays/08457-vbgl-simplex.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$CI_PULL_REQUEST" = "8457" ]; then
+if [ "$CI_PULL_REQUEST" = "8457" ] || [ "$CI_BRANCH" = "simplex" ]; then
         fiat_crypto_CI_REF=simplex
         fiat_crypto_CI_GITURL=https://github.com/vbgl/fiat-crypto
 fi
