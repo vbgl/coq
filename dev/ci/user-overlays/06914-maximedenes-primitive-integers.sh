860cd5eeedf543ce94505136a98e7ad37cf09b10
Vincent Laporte <Vincent.Laporte@fondation-inria.fr>
2018-10-17 09:47:21 +0000

    overlay

--- dev/ci/user-overlays/06914-maximedenes-primitive-integers.sh
+++ dev/ci/user-overlays/06914-maximedenes-primitive-integers.sh
@@ -1,4 +1,4 @@
 if [ "$CI_PULL_REQUEST" = "6914" ] || [ "$CI_BRANCH" = "primitive-integers" ]; then
     bignums_CI_BRANCH=primitive-integers
-    bignums_CI_GITURL=https://github.com/maximedenes/bignums.git
+    bignums_CI_GITURL=https://github.com/vbgl/bignums.git
 fi
