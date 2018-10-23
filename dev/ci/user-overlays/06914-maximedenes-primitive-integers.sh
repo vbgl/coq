3206de08e4be1ae4762c73ebbbe25dfda112b891
Vincent Laporte <Vincent.Laporte@fondation-inria.fr>
2018-10-23 10:15:17 +0000

    Overlay for CoLoR

--- dev/ci/user-overlays/06914-maximedenes-primitive-integers.sh
+++ dev/ci/user-overlays/06914-maximedenes-primitive-integers.sh
@@ -3,6 +3,9 @@ if [ "$CI_PULL_REQUEST" = "6914" ] || [ "$CI_BRANCH" = "primitive-integers" ]; t
     bignums_CI_REF=primitive-integers
     bignums_CI_GITURL=https://github.com/vbgl/bignums
 
+    CoLoR_CI_REF=primitive-integers
+    CoLoR_CI_GITURL=https://github.com/vbgl/color
+
     CompCert_CI_REF=primitive-integers
     CompCert_CI_GITURL=https://github.com/vbgl/CompCert
 
