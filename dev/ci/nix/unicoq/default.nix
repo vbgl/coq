46e511686a2fc94a986dc38959d5efcf14a3d720
Vincent Laporte <Vincent.Laporte@fondation-inria.fr>
2019-07-24 09:48:17 +0000

    [Nix-CI] Remove local patch
    A similar patch has been merged upstream
    (see https://github.com/unicoq/unicoq/pull/30/).

--- dev/ci/nix/unicoq/default.nix
+++ dev/ci/nix/unicoq/default.nix
@@ -10,8 +10,6 @@ stdenv.mkDerivation {
   name = "coq${coq.coq-version}-unicoq-0.0-git";
   src = fetchTarball https://github.com/unicoq/unicoq/archive/master.tar.gz;
 
-  patches = [ ./unicoq-num.patch ];
-
   buildInputs = [ coq ] ++ (with coq.ocamlPackages; [ ocaml findlib camlp5 num ]);
 
   configurePhase = "coq_makefile -f Make -o Makefile";
