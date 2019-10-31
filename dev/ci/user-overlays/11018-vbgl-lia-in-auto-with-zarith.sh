04ba51625b2760413c790315285e7851b76e9258
Vincent Laporte <Vincent.Laporte@inria.fr>
2019-10-31 15:21:35 +0000

    Overlay for CompCert

--- dev/ci/user-overlays/11018-vbgl-lia-in-auto-with-zarith.sh
+++ dev/ci/user-overlays/11018-vbgl-lia-in-auto-with-zarith.sh
@@ -1,3 +1,6 @@
 if [ "$CI_PULL_REQUEST" = "11018" ] || [ "$CI_BRANCH" = "lia-in-auto-with-zarith" ]; then
 
+    compcert_CI_REF=auto-with-zarith
+    compcert_CI_GITURL=https://github.com/vbgl/CompCert
+
 fi
