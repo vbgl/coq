ce274cf454f342157caa2bc512f5f615f180639e
Vincent Laporte <Vincent.Laporte@inria.fr>
2019-11-04 14:29:39 +0000

    Overlay for bignums

--- dev/ci/user-overlays/11018-vbgl-lia-in-auto-with-zarith.sh
+++ dev/ci/user-overlays/11018-vbgl-lia-in-auto-with-zarith.sh
@@ -1,5 +1,8 @@
 if [ "$CI_PULL_REQUEST" = "11018" ] || [ "$CI_BRANCH" = "lia-in-auto-with-zarith" ]; then
 
+    bignums_CI_REF=auto-with-zarith
+    bignums_CI_GITURL=https://github.com/vbgl/bignums
+
     compcert_CI_REF=auto-with-zarith
     compcert_CI_GITURL=https://github.com/vbgl/CompCert
 
