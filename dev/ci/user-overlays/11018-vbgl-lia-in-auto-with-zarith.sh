c44116350423920e6961bb3c298e89bcd40d057b
Vincent Laporte <Vincent.Laporte@inria.fr>
2019-11-04 16:11:38 +0000

    Overlay for fiat-crypto-legacy

--- dev/ci/user-overlays/11018-vbgl-lia-in-auto-with-zarith.sh
+++ dev/ci/user-overlays/11018-vbgl-lia-in-auto-with-zarith.sh
@@ -6,4 +6,6 @@ if [ "$CI_PULL_REQUEST" = "11018" ] || [ "$CI_BRANCH" = "lia-in-auto-with-zarith
     compcert_CI_REF=auto-with-zarith
     compcert_CI_GITURL=https://github.com/vbgl/CompCert
 
+    fiat_crypto_legacy_CI_GITURL=https://github.com/vbgl/fiat-crypto
+
 fi
