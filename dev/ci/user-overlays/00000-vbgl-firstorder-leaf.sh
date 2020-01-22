88a4c56b2f34b745f83d5e82e81f51419f55d9d5
Vincent Laporte <Vincent.Laporte@inria.fr>
2020-01-22 10:34:23 +0100

    Overlay for coq-dpdgraph

--- dev/ci/user-overlays/00000-vbgl-firstorder-leaf.sh
+++ dev/ci/user-overlays/00000-vbgl-firstorder-leaf.sh
@@ -1,3 +1,6 @@
 if [ "$CI_PULL_REQUEST" = "98765" ] || [ "$CI_BRANCH" = "firstorder-leaf-is-idtac" ]; then
 
+coq_dpdgraph_CI_GITURL=https://github.com/vbgl/coq-dpdgraph
+coq_dpdgraph_CI_REF=firstorder-leaf
+
 fi
