70449e1967d41a9e75e1090f33b42e2acb89b652
Vincent Laporte <Vincent.Laporte@inria.fr>
2020-01-22 13:59:47 +0100

    Overlay for aac-tactics

--- dev/ci/user-overlays/00000-vbgl-firstorder-leaf.sh
+++ dev/ci/user-overlays/00000-vbgl-firstorder-leaf.sh
@@ -1,5 +1,8 @@
 if [ "$CI_PULL_REQUEST" = "98765" ] || [ "$CI_BRANCH" = "firstorder-leaf-is-idtac" ]; then
 
+aac_tactics_CI_GITURL=https://github.com/vbgl/aac-tactics
+aac_tactics_CI_REF=firstorder-leaf
+
 coq_dpdgraph_CI_GITURL=https://github.com/vbgl/coq-dpdgraph
 coq_dpdgraph_CI_REF=firstorder-leaf
 
